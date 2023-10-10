0f35b155a88c02baf6cedeaa1d2a9ef29763b58d
ajeetsigma <ajkumar@sigmasolve.net>
2023-10-10 19:59:06 +0530

    implement file upload middleware

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,15 @@
-import express from "express"
-import * as dotenv from "dotenv"
-import { V1Routes } from "./apis"
-dotenv.config()
-const port=process.env.PORT|| 3000
-export const webServer =() =>{
-const app= express()
-app.use(express.json())
-const v1 = new V1Routes()
-app.use('/api/v1', v1.path())
+import express from "express";
+import * as dotenv from "dotenv";
+import { V1Routes } from "./apis";
+dotenv.config();
+const port = process.env.PORT || 3000;
+export const webServer = () => {
+  const app = express();
+  app.use(express.json());
+  const v1 = new V1Routes();
+  app.use("/api/v1", v1.path());
 
-app.listen(port,()=>{
-  console.log(`server is running http://localhost:${port}`)
-})
-}
+  app.listen(port, () => {
+    console.log(`server is running http://localhost:${port}`);
+  });
+};
